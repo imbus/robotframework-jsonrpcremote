d1e485fee1e7b5c88414d4787e031f9cc21d86cb
Daniel Biehl <dbiehl@live.de>
2025-12-19 00:17:24 +0100

    chore: set version to 0.1.0 and remove dynamic version retrieval

--- src/JsonRpcRemote/__version__.py
+++ src/JsonRpcRemote/__version__.py
@@ -1,6 +1,1 @@
-import importlib
-
-try:
-    __version__ = importlib.metadata.version("robotframework-jsonrpcremote")
-except importlib.metadata.PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = "0.1.0"
